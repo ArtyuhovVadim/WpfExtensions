a0033cbf47cdb47580aea922400677174aa32a12
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-05-10 00:45:51 +0300

    Добавил RowDefinitionsExProperty

--- WpfExtensions/AttachedDependencyProperties/GridProperties.cs
+++ WpfExtensions/AttachedDependencyProperties/GridProperties.cs
@@ -7,6 +7,12 @@ namespace WpfExtensions.AttachedDependencyProperties;
 
 public static class GridProperties
 {
+    private enum RowOrColumn
+    {
+        Row,
+        Column
+    }
+
     private static readonly char[] Separators = { ',', ' ' };
 
     private const char GroupSeparator = ':';
@@ -15,10 +21,12 @@ public static class GridProperties
 
     private const char StarChar = '*';
 
-    public static readonly DependencyProperty ColumnDefinitionsExProperty =
-        DependencyProperty.RegisterAttached("ColumnDefinitionsEx", typeof(string), typeof(GridProperties), new PropertyMetadata(string.Empty, OnPropertyChanged));
+    #region ColumnDefinitionsExProperty
 
-    private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    public static readonly DependencyProperty ColumnDefinitionsExProperty =
+        DependencyProperty.RegisterAttached("ColumnDefinitionsEx", typeof(string), typeof(GridProperties), new PropertyMetadata(string.Empty, OnColumnDefinitionsExPropertyChanged));
+
+    private static void OnColumnDefinitionsExPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         if (d is not Grid grid)
             throw new InvalidOperationException($"{nameof(ColumnDefinitionsExProperty)} can not be set for {d.GetType()}!");
@@ -32,29 +40,12 @@ public static class GridProperties
 
         var columnStrings = columnsString.Split(Separators, StringSplitOptions.TrimEntries);
 
-        foreach (var columnString in columnStrings)
-        {
-            if (columnString.Contains(GroupSeparator))
-            {
-                var columnParts = columnString.Split(GroupSeparator, StringSplitOptions.TrimEntries);
-
-                if (columnParts.Length != 2)
-                    throw new ArgumentException($"Can not parse '{columnString}' string!");
-
-                var column = columnParts[0];
-                var groupGroupName = columnParts[1];
-
-                AddColumnToGrid(grid, ParseGridLength(column), groupGroupName);
-            }
-            else
-            {
-                AddColumnToGrid(grid, ParseGridLength(columnString));
-            }
-        }
+        AddRowsOrColumnsToGrid(grid, columnStrings, RowOrColumn.Column);
     }
 
     /// <summary>
     /// Set <see cref="Grid.ColumnDefinitions"/> as string.
+    /// <example>
     /// <c>
     /// <list type="bullet">
     /// <item>Use '*' for &lt;ColumnDefinition Width="*"&gt;</item>
@@ -73,6 +64,7 @@ public static class GridProperties
     /// <item>"*:AGroup 1.5* 2* Auto:BGroup 300"</item>
     /// </list>
     /// </c>
+    /// </example>
     /// </summary>
     /// <param name="o">Grid object</param>
     /// <param name="value">ColumnsDefinition as string</param>
@@ -86,6 +78,99 @@ public static class GridProperties
     /// <param name="o">Grid object</param>
     /// <returns><see cref="Grid.ColumnDefinitions"/> as string</returns>
     public static string GetColumnDefinitionsEx(DependencyObject o) => (string)o.GetValue(ColumnDefinitionsExProperty);
+
+    #endregion
+
+    #region RowDefinitionsExProperty
+
+    public static readonly DependencyProperty RowDefinitionsExProperty =
+        DependencyProperty.RegisterAttached("RowDefinitionsEx", typeof(string), typeof(GridProperties), new PropertyMetadata(string.Empty, OnRowDefinitionsExPropertyChanged));
+
+    private static void OnRowDefinitionsExPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not Grid grid)
+            throw new InvalidOperationException($"{nameof(RowDefinitionsExProperty)} can not be set for {d.GetType()}!");
+
+        grid.RowDefinitions.Clear();
+
+        var rowsString = (string)e.NewValue;
+
+        if (string.IsNullOrWhiteSpace(rowsString))
+            throw new ArgumentException("Rows string is null or white space!");
+
+        var rowStrings = rowsString.Split(Separators, StringSplitOptions.TrimEntries);
+
+        AddRowsOrColumnsToGrid(grid, rowStrings, RowOrColumn.Row);
+    }
+
+    /// <summary>
+    /// Set <see cref="Grid.RowDefinitions"/> as string.
+    /// <example>
+    /// <c>
+    /// <list type="bullet">
+    /// <item>Use '*' for &lt;RowDefinition Height="*"&gt;</item>
+    /// <item>Use '1.5*' for &lt;RowDefinition Height="1.5*"&gt;</item>
+    /// <item>Use 'Auto' for &lt;RowDefinition Height="Auto"&gt;</item>
+    /// <item>Use '300' for &lt;RowDefinition Height="300"&gt;</item>
+    /// <item>Use '*:SomeName' for &lt;RowDefinition Height="*" SharedSizeGroup="SomeName"&gt;</item>
+    /// <item>Use '1.5*:SomeName' for &lt;RowDefinition Height="1.5*" SharedSizeGroup="SomeName"&gt;</item>
+    /// <item>Use 'Auto:SomeName' for &lt;RowDefinition Height="Auto" SharedSizeGroup="SomeName"&gt;</item>
+    /// <item>Use '300:SomeName' for &lt;RowDefinition Height="300" SharedSizeGroup="SomeName"&gt;</item>
+    /// </list>
+    /// Example strings:
+    /// <list type="table">
+    /// <item>"*, 1.5*, 2*, Auto, 300"</item>
+    /// <item>"* 1.5* 2* Auto 300"</item>
+    /// <item>"*:AGroup 1.5* 2* Auto:BGroup 300"</item>
+    /// </list>
+    /// </c>
+    /// </example>
+    /// </summary>
+    /// <param name="o">Grid object</param>
+    /// <param name="value">RowsDefinition as string</param>
+    /// <exception cref="InvalidOperationException">An exception is thrown if you try to set a property other than <see cref="Grid"/></exception>
+    /// <exception cref="ArgumentException">An exception is thrown if input <see cref="string"/> incorrect</exception>
+    public static void SetRowDefinitionsEx(DependencyObject o, string value) => o.SetValue(RowDefinitionsExProperty, value);
+
+    /// <summary>
+    /// Returns <see cref="Grid.RowDefinitions"/> as string.
+    /// </summary>
+    /// <param name="o">Grid object</param>
+    /// <returns><see cref="Grid.RowDefinitions"/> as string</returns>
+    public static string GetRowDefinitionsEx(DependencyObject o) => (string)o.GetValue(RowDefinitionsExProperty);
+
+    #endregion
+
+    private static void AddRowsOrColumnsToGrid(Grid grid, string[] strings, RowOrColumn rowOrColumn)
+    {
+        foreach (var str in strings)
+        {
+            if (str.Contains(GroupSeparator))
+            {
+                var parts = str.Split(GroupSeparator, StringSplitOptions.TrimEntries);
+
+                if (parts.Length != 2)
+                    throw new ArgumentException($"Can not parse '{str}' string!");
+
+                var value = parts[0];
+                var groupGroupName = parts[1];
+
+                switch (rowOrColumn)
+                {
+                    case RowOrColumn.Row: AddRowToGrid(grid, ParseGridLength(value), groupGroupName); break;
+                    case RowOrColumn.Column: AddColumnToGrid(grid, ParseGridLength(value), groupGroupName); break;
+                }
+            }
+            else
+            {
+                switch (rowOrColumn)
+                {
+                    case RowOrColumn.Row: AddRowToGrid(grid, ParseGridLength(str)); break;
+                    case RowOrColumn.Column: AddColumnToGrid(grid, ParseGridLength(str)); break;
+                }
+            }
+        }
+    }
 
     private static GridLength ParseGridLength(string columnString)
     {
@@ -122,5 +207,15 @@ public static class GridProperties
     private static void AddColumnToGrid(Grid grid, GridLength length, string groupName)
     {
         grid.ColumnDefinitions.Add(new ColumnDefinition { Width = length, SharedSizeGroup = groupName });
+    }
+
+    private static void AddRowToGrid(Grid grid, GridLength length)
+    {
+        grid.RowDefinitions.Add(new RowDefinition { Height = length });
+    }
+
+    private static void AddRowToGrid(Grid grid, GridLength length, string groupName)
+    {
+        grid.RowDefinitions.Add(new RowDefinition { Height = length, SharedSizeGroup = groupName });
     }
 }
