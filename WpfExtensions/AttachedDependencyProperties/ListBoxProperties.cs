55d12a3d7839ec1a4aa053b4c5a6b6e0930e919c
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-05-11 00:17:56 +0300

    Добавил AutoScrollToSelectedItemProperty для ListBox

---
+++ WpfExtensions/AttachedDependencyProperties/ListBoxProperties.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System;
+
+namespace WpfExtensions.AttachedDependencyProperties;
+
+public static class ListBoxProperties
+{
+    #region AutoScrollToSelectedItemProperty
+
+    public static readonly DependencyProperty AutoScrollToSelectedItemProperty =
+        DependencyProperty.RegisterAttached("AutoScrollToSelectedItem", typeof(bool), typeof(ListBoxProperties), new PropertyMetadata(false, OnAutoScrollToSelectedItemPropertyChanged));
+
+    private static void OnAutoScrollToSelectedItemPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not ListBox selector)
+            throw new InvalidOperationException($"{nameof(AutoScrollToSelectedItemProperty)} can not be set for {d.GetType()}!");
+
+        var newValue = (bool)e.NewValue;
+
+        if (newValue) selector.SelectionChanged += OnSelectionChanged;
+        else selector.SelectionChanged -= OnSelectionChanged;
+    }
+
+    private static void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var listBox = (ListBox)sender;
+
+        listBox.ScrollIntoView(listBox.SelectedItem);
+    }
+
+    public static void SetAutoScrollToSelectedItem(DependencyObject o, bool value) => o.SetValue(AutoScrollToSelectedItemProperty, value);
+
+    public static bool GetAutoScrollToSelectedItem(DependencyObject o) => (bool)o.GetValue(AutoScrollToSelectedItemProperty);
+
+    #endregion
+}
