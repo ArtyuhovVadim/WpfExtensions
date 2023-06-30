09b1cb1c2e2907d44394cc01ec4b3576eadae9bd
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-07-01 02:18:32 +0300

    Добавил ColorPicker

---
+++ WpfExtensions/Controls/ColorPicker.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfExtensions.Controls;
+
+[TemplatePart(Name = ApplyButtonName, Type = typeof(Button))]
+[TemplatePart(Name = CancelButtonName, Type = typeof(Button))]
+public class ColorPicker : Control
+{
+    private const string ApplyButtonName = "PART_ApplyButton";
+    private const string CancelButtonName = "PART_CancelButton";
+
+    private ICommand _selectedColorCommand;
+
+    private Button _applyButton;
+    private Button _cancelButton;
+
+    static ColorPicker()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorPicker), new FrameworkPropertyMetadata(typeof(ColorPicker)));
+    }
+
+    #region SelectedColor
+
+    public Color SelectedColor
+    {
+        get => (Color)GetValue(SelectedColorProperty);
+        set => SetValue(SelectedColorProperty, value);
+    }
+
+    public static readonly DependencyProperty SelectedColorProperty =
+        DependencyProperty.Register(nameof(SelectedColor), typeof(Color), typeof(ColorPicker), new PropertyMetadata(Colors.Black));
+
+    #endregion
+
+    #region CurrentColor
+
+    public Color CurrentColor
+    {
+        get => (Color)GetValue(CurrentColorProperty);
+        set => SetValue(CurrentColorProperty, value);
+    }
+
+    public static readonly DependencyProperty CurrentColorProperty =
+        DependencyProperty.Register(nameof(CurrentColor), typeof(Color), typeof(ColorPicker), new PropertyMetadata(Colors.Black, OnCurrentColorChanged));
+
+    private static void OnCurrentColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not ColorPicker picker) return;
+
+        var color = (Color)e.NewValue;
+
+        if (!picker.IsTransparencySupported && color.A != 255)
+            color.A = 255;
+
+        picker.RComponent = color.R;
+        picker.GComponent = color.G;
+        picker.BComponent = color.B;
+
+        picker.HexColor = ColorToString(color, picker.IsTransparencySupported);
+    }
+
+    #endregion
+
+    #region IsTransparencySupported
+
+    public bool IsTransparencySupported
+    {
+        get => (bool)GetValue(IsTransparencySupportedProperty);
+        set => SetValue(IsTransparencySupportedProperty, value);
+    }
+
+    public static readonly DependencyProperty IsTransparencySupportedProperty =
+        DependencyProperty.Register(nameof(IsTransparencySupported), typeof(bool), typeof(ColorPicker), new PropertyMetadata(false));
+
+    #endregion
+
+    #region HexColor
+
+    public string HexColor
+    {
+        get => (string)GetValue(HexColorProperty);
+        set => SetValue(HexColorProperty, value);
+    }
+
+    public static readonly DependencyProperty HexColorProperty =
+        DependencyProperty.Register(nameof(HexColor), typeof(string), typeof(ColorPicker), new PropertyMetadata(string.Empty, OnHexColorChanged, OnCoerceHexColor));
+
+    private static object OnCoerceHexColor(DependencyObject d, object basevalue)
+    {
+        if (d is not ColorPicker picker) return basevalue;
+
+        return ColorToString(StringToColor((string)basevalue, picker.IsTransparencySupported, picker.CurrentColor), picker.IsTransparencySupported);
+    }
+
+    private static void OnHexColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not ColorPicker picker) return;
+
+        picker.CurrentColor = StringToColor((string)e.NewValue, picker.IsTransparencySupported, Colors.Black);
+    }
+
+    #endregion
+
+    #region RComponent
+
+    public int RComponent
+    {
+        get => (int)GetValue(RComponentProperty);
+        set => SetValue(RComponentProperty, value);
+    }
+
+    public static readonly DependencyProperty RComponentProperty =
+        DependencyProperty.Register(nameof(RComponent), typeof(int), typeof(ColorPicker), new PropertyMetadata(default(int), OnColorComponentChanged, OnCoerceColorComponentValue));
+
+    #endregion
+
+    #region GComponent
+
+    public int GComponent
+    {
+        get => (int)GetValue(GComponentProperty);
+        set => SetValue(GComponentProperty, value);
+    }
+
+    public static readonly DependencyProperty GComponentProperty =
+        DependencyProperty.Register(nameof(GComponent), typeof(int), typeof(ColorPicker), new PropertyMetadata(default(int), OnColorComponentChanged, OnCoerceColorComponentValue));
+
+    #endregion
+
+    #region BComponent
+
+    public int BComponent
+    {
+        get => (int)GetValue(BComponentProperty);
+        set => SetValue(BComponentProperty, value);
+    }
+
+    public static readonly DependencyProperty BComponentProperty =
+        DependencyProperty.Register(nameof(BComponent), typeof(int), typeof(ColorPicker), new PropertyMetadata(default(int), OnColorComponentChanged, OnCoerceColorComponentValue));
+
+    #endregion
+
+    public ICommand SelectedColorCommand => _selectedColorCommand ??= new LambdaCommand(p =>
+    {
+        if (p is not Color color) return;
+
+        OnColorSelected(color);
+    });
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        _applyButton = GetTemplateChild(ApplyButtonName) as Button ?? throw new ElementNotAvailableException($"{ApplyButtonName} is not available in template");
+        _cancelButton = GetTemplateChild(CancelButtonName) as Button ?? throw new ElementNotAvailableException($"{CancelButtonName} is not available in template");
+
+        CurrentColor = Colors.Red;
+        CurrentColor = Colors.Black;
+    }
+
+    private void OnColorSelected(Color color)
+    {
+        CurrentColor = color;
+        SelectedColor = CurrentColor;
+    }
+
+    private static void OnColorComponentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not ColorPicker picker) return;
+
+        picker.CurrentColor = Color.FromRgb((byte)picker.RComponent, (byte)picker.GComponent, (byte)picker.BComponent);
+    }
+
+    private static object OnCoerceColorComponentValue(DependencyObject d, object basevalue)
+    {
+        if (d is not ColorPicker) return basevalue;
+
+        if (int.TryParse(basevalue.ToString(), out var result))
+        {
+            return Math.Min(Math.Max(0, result), 255);
+        }
+
+        return 0;
+    }
+
+    private static string ColorToString(Color color, bool isTransparencySupported)
+    {
+        if (!isTransparencySupported)
+            return color.ToString().Remove(1, 2);
+
+        return color.ToString();
+    }
+
+    private static Color StringToColor(string str, bool isTransparencySupported, Color fallbackColor)
+    {
+        var pattern = $$"""#[0-9A-F]{{{(isTransparencySupported ? 8 : 6)}}}$""";
+        var matchResult = Regex.Match(str, pattern);
+
+        if (!matchResult.Success)
+            return fallbackColor;
+
+        return (Color)ColorConverter.ConvertFromString(str)!;
+    }
+}
---
+++ WpfExtensions/Controls/PaletteBrush.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+
+namespace WpfExtensions.Controls;
+
+public class PaletteBrush
+{
+    public static readonly List<PaletteBrush> Palettes = GetPaletteBrushes();
+
+    public Brush GrayscaleBrush { get; set; }
+
+    public Brush MainBrush { get; set; }
+
+    public List<Brush> Brushes { get; set; }
+
+    private static List<PaletteBrush> GetPaletteBrushes()
+    {
+        var list = new List<PaletteBrush>
+        {
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#980000"),
+                GrayscaleBrush = CreateBrushFromHex("#000000"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#e6b8af"),
+                    CreateBrushFromHex("#dd7e6b"),
+                    CreateBrushFromHex("#cc4125"),
+                    CreateBrushFromHex("#a61c00"),
+                    CreateBrushFromHex("#85200c"),
+                    CreateBrushFromHex("#5b0f00")
+                }
+            },
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#ff0000"),
+                GrayscaleBrush = CreateBrushFromHex("#434343"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#f4cccc"),
+                    CreateBrushFromHex("#ea9999"),
+                    CreateBrushFromHex("#e06666"),
+                    CreateBrushFromHex("#cc0000"),
+                    CreateBrushFromHex("#990000"),
+                    CreateBrushFromHex("#660000")
+                }
+            },
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#ff9900"),
+                GrayscaleBrush = CreateBrushFromHex("#666666"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#fce5cd"),
+                    CreateBrushFromHex("#f9cb9c"),
+                    CreateBrushFromHex("#f6b26b"),
+                    CreateBrushFromHex("#e69138"),
+                    CreateBrushFromHex("#b45f06"),
+                    CreateBrushFromHex("#783f04")
+                }
+            },
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#ffff00"),
+                GrayscaleBrush = CreateBrushFromHex("#999999"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#fff2cc"),
+                    CreateBrushFromHex("#ffe599"),
+                    CreateBrushFromHex("#ffd966"),
+                    CreateBrushFromHex("#f1c232"),
+                    CreateBrushFromHex("#bf9000"),
+                    CreateBrushFromHex("#7f6000")
+                }
+            },
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#00ff00"),
+                GrayscaleBrush = CreateBrushFromHex("#b7b7b7"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#d9ead3"),
+                    CreateBrushFromHex("#b6d7a8"),
+                    CreateBrushFromHex("#93c47d"),
+                    CreateBrushFromHex("#6aa84f"),
+                    CreateBrushFromHex("#38761d"),
+                    CreateBrushFromHex("#274e13")
+                }
+            },
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#00ffff"),
+                GrayscaleBrush = CreateBrushFromHex("#cccccc"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#d0e0e3"),
+                    CreateBrushFromHex("#a2c4c9"),
+                    CreateBrushFromHex("#76a5af"),
+                    CreateBrushFromHex("#45818e"),
+                    CreateBrushFromHex("#134f5c"),
+                    CreateBrushFromHex("#0c343d")
+                }
+            },
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#4a86e8"),
+                GrayscaleBrush = CreateBrushFromHex("#d9d9d9"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#c9daf8"),
+                    CreateBrushFromHex("#a4c2f4"),
+                    CreateBrushFromHex("#6d9eeb"),
+                    CreateBrushFromHex("#3c78d8"),
+                    CreateBrushFromHex("#1155cc"),
+                    CreateBrushFromHex("#1c4587")
+                }
+            },
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#0000ff"),
+                GrayscaleBrush = CreateBrushFromHex("#efefef"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#cfe2f3"),
+                    CreateBrushFromHex("#9fc5e8"),
+                    CreateBrushFromHex("#6fa8dc"),
+                    CreateBrushFromHex("#3d85c6"),
+                    CreateBrushFromHex("#0b5394"),
+                    CreateBrushFromHex("#073763")
+                }
+            },
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#9900ff"),
+                GrayscaleBrush = CreateBrushFromHex("#f3f3f3"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#d9d2e9"),
+                    CreateBrushFromHex("#b4a7d6"),
+                    CreateBrushFromHex("#8e7cc3"),
+                    CreateBrushFromHex("#674ea7"),
+                    CreateBrushFromHex("#351c75"),
+                    CreateBrushFromHex("#20124d")
+                }
+            },
+            new()
+            {
+                MainBrush = CreateBrushFromHex("#ff00ff"),
+                GrayscaleBrush = CreateBrushFromHex("#ffffff"),
+                Brushes = new List<Brush>
+                {
+                    CreateBrushFromHex("#ead1dc"),
+                    CreateBrushFromHex("#d5a6bd"),
+                    CreateBrushFromHex("#c27ba0"),
+                    CreateBrushFromHex("#a64d79"),
+                    CreateBrushFromHex("#741b47"),
+                    CreateBrushFromHex("#4c1130")
+                }
+            }
+        };
+
+        foreach (var brush in list.SelectMany(item => item.Brushes))
+        {
+            brush.Freeze();
+        }
+
+        return list;
+    }
+
+    private static SolidColorBrush CreateBrushFromHex(string hex)
+    {
+        return (SolidColorBrush)new BrushConverter().ConvertFrom(hex);
+    }
+}
