17b041214852cd5430b3edbceb56a8f0b99072c5
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-07-19 22:32:09 +0300

    Изменил DemoApp

--- DemoApp/MainWindow.xaml.cs
+++ DemoApp/MainWindow.xaml.cs
@@ -1,21 +1,6 @@
-﻿using System.Diagnostics;
-using System.Windows;
-using System.Windows.Media;
-using WpfExtensions;
-
-namespace DemoApp;
+﻿namespace DemoApp;
 
 public partial class MainWindow
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-
-        TestButton.Click += OnTestButtonClick;
-    }
-
-    private void OnTestButtonClick(object sender, RoutedEventArgs e)
-    {
-        HsvColorPicker.Color = Colors.Aqua;
-    }
+    public MainWindow() => InitializeComponent();
 }
