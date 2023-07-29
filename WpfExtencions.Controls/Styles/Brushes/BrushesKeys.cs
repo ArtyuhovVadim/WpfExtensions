5391e344b79fa68fa665ba9e56bb43b3c201aa05
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-07-29 23:15:49 +0300

    Добавил первый ресурс

---
+++ WpfExtencions.Controls/Styles/Brushes/BrushesKeys.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WpfExtensions.Controls.Styles.Brushes;
+
+public static class BrushesKeys
+{
+    public static readonly ComponentResourceKey ColorPickerControlBackground = CreateInstance();
+
+    private static ComponentResourceKey CreateInstance([CallerMemberName] string fieldName = null!)
+    {
+        return new ComponentResourceKey(typeof(BrushesKeys), fieldName!);
+    }
+}
