358b4fc38c511a46afc7b5ab0adcab7f57eb6d42
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-07-30 13:17:07 +0300

    Добавил все кисти для ColorPicker

--- WpfExtencions.Controls/Styles/Brushes/BrushesKeys.cs
+++ WpfExtencions.Controls/Styles/Brushes/BrushesKeys.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
 using System.Windows;
 
 namespace WpfExtensions.Controls.Styles.Brushes;
@@ -10,10 +10,27 @@ public static class BrushesKeys
         public static readonly ComponentResourceKey Background = CreateInstance();
 
         public static readonly ComponentResourceKey Foreground = CreateInstance();
+
+        public static readonly ComponentResourceKey BorderBrush = CreateInstance();
     }
 
-    private static ComponentResourceKey CreateInstance([CallerMemberName] string fieldName = null!)
+    public static class MinimizedColorPickerControl
     {
-        return new ComponentResourceKey(typeof(BrushesKeys), fieldName);
+        public static readonly ComponentResourceKey Background = CreateInstance();
+
+        public static readonly ComponentResourceKey Foreground = CreateInstance();
+
+        public static readonly ComponentResourceKey BorderBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey MinimizedBackground = CreateInstance();
+
+        public static readonly ComponentResourceKey MinimizedBorderBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey MouseOverBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey FocusedBrush = CreateInstance();
     }
+
+    private static ComponentResourceKey CreateInstance() => 
+        new(typeof(BrushesKeys), Guid.NewGuid());
 }
