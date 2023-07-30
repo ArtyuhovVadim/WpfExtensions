465a869dc8cb5d1a41c249049bcae5b32038486b
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-07-30 15:50:35 +0300

    Перенес все кисти в один файл

--- WpfExtencions.Controls/Styles/Brushes/BrushesKeys.cs
+++ WpfExtencions.Controls/Styles/Brushes/BrushesKeys.cs
@@ -31,6 +31,47 @@ public static class BrushesKeys
         public static readonly ComponentResourceKey FocusedBrush = CreateInstance();
     }
 
-    private static ComponentResourceKey CreateInstance() => 
+    public static class ColorPalette
+    {
+        public static readonly ComponentResourceKey Background = CreateInstance();
+
+        public static readonly ComponentResourceKey BorderBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey HighlightColor = CreateInstance();
+    }
+
+    public static class ColorSlider
+    {
+        public static readonly ComponentResourceKey Background = CreateInstance();
+
+        public static readonly ComponentResourceKey BorderBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey ThumbBrush = CreateInstance();
+    }
+
+    public static class HsvColorPicker
+    {
+        public static readonly ComponentResourceKey Background = CreateInstance();
+
+        public static readonly ComponentResourceKey BorderBrush = CreateInstance();
+    }
+
+    public static class RecentBrushes
+    {
+        public static readonly ComponentResourceKey Background = CreateInstance();
+
+        public static readonly ComponentResourceKey BorderBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey HighlightColor = CreateInstance();
+    }
+
+    public static class TextColorPicker
+    {
+        public static readonly ComponentResourceKey Background = CreateInstance();
+
+        public static readonly ComponentResourceKey BorderBrush = CreateInstance();
+    }
+
+    private static ComponentResourceKey CreateInstance() =>
         new(typeof(BrushesKeys), Guid.NewGuid());
 }
