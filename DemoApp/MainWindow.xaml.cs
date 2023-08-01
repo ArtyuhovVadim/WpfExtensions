c1b9c6edfad573f17b9f8c40b75e416941367462
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-08-01 22:38:35 +0300

    Очистил тестовое приложение

--- DemoApp/MainWindow.xaml.cs
+++ DemoApp/MainWindow.xaml.cs
@@ -1,28 +1,6 @@
-﻿using System.Windows;
-using System.Windows.Input;
-
-namespace DemoApp;
+﻿namespace DemoApp;
 
 public partial class MainWindow
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-
-        KeyDown += OnKeyDown;
-    }
-
-    private void OnKeyDown(object sender, KeyEventArgs e)
-    {
-        if (e.Key == Key.Enter)
-        {
-            
-
-            Application.Current.Resources.MergedDictionaries.RemoveAt(0);
-            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("/WpfExtensions.Controls;component/Styles/Brushes/LightBrushes.xaml", UriKind.Relative) });
-            var a = Application.Current.Resources.MergedDictionaries;
-
-
-        }
-    }
+    public MainWindow() => InitializeComponent();
 }
