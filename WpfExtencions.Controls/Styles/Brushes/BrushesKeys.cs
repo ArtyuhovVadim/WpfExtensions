33a68f230b3897ea5744407dfc2e052f3314b0da
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-08-03 23:11:40 +0300

    Добавил стиль для PasswordBox

--- WpfExtencions.Controls/Styles/Brushes/BrushesKeys.cs
+++ WpfExtencions.Controls/Styles/Brushes/BrushesKeys.cs
@@ -323,6 +323,27 @@ public static class BrushesKeys
         public static readonly ComponentResourceKey SelectionRangeBrush = CreateInstance();
     }
 
+    public static class PasswordBox
+    {
+        public static readonly ComponentResourceKey Background = CreateInstance();
+
+        public static readonly ComponentResourceKey Foreground = CreateInstance();
+
+        public static readonly ComponentResourceKey BorderBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey SelectionBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey CaretBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey FocusedBorderBrush = CreateInstance();
+
+        public static readonly ComponentResourceKey DisabledBackground = CreateInstance();
+
+        public static readonly ComponentResourceKey DisabledForeground = CreateInstance();
+
+        public static readonly ComponentResourceKey DisabledBorderBrush = CreateInstance();
+    }
+
     private static ComponentResourceKey CreateInstance() =>
         new(typeof(BrushesKeys), Guid.NewGuid());
 }
