0965a6bc0d66806ddf07db9f93ecf040cfd79f1e
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-08-06 22:44:18 +0300

    Добавил стиль для ProgressBar

--- WpfExtencions.Controls/Styles/Brushes/BrushesKeys.cs
+++ WpfExtencions.Controls/Styles/Brushes/BrushesKeys.cs
@@ -424,6 +424,15 @@ public static class BrushesKeys
         public static readonly ComponentResourceKey SelectedForeground = CreateInstance();
     }
 
+    public static class ProgressBar
+    {
+        public static readonly ComponentResourceKey Background = CreateInstance();
+
+        public static readonly ComponentResourceKey Foreground = CreateInstance();
+
+        public static readonly ComponentResourceKey BorderBrush = CreateInstance();
+    }
+
     private static ComponentResourceKey CreateInstance() =>
         new(typeof(BrushesKeys), Guid.NewGuid());
 }
