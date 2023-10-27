138ed11ceb1c8f93e634e69fcae0f9c4a55a37ca
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-10-27 22:02:28 +0300

    Переименовал Spacing в Gap и поменял поведение свойства

--- WpfExtencions.Controls/StackPanelEx.cs
+++ WpfExtencions.Controls/StackPanelEx.cs
@@ -5,17 +5,17 @@ namespace WpfExtensions.Controls;
 
 public class StackPanelEx : StackPanel
 {
-    #region Spacing
+    #region Gap
 
-    public Thickness Spacing
+    public double Gap
     {
-        get => (Thickness)GetValue(SpacingProperty);
-        set => SetValue(SpacingProperty, value);
+        get => (double)GetValue(GapProperty);
+        set => SetValue(GapProperty, value);
     }
 
-    public static readonly DependencyProperty SpacingProperty =
-        DependencyProperty.Register(nameof(Spacing), typeof(Thickness), typeof(StackPanelEx),
-            new FrameworkPropertyMetadata(new Thickness(), FrameworkPropertyMetadataOptions.AffectsMeasure));
+    public static readonly DependencyProperty GapProperty =
+        DependencyProperty.Register(nameof(Gap), typeof(double), typeof(StackPanelEx),
+            new FrameworkPropertyMetadata(0d, FrameworkPropertyMetadataOptions.AffectsMeasure));
 
     #endregion
 
@@ -29,16 +29,25 @@ public class StackPanelEx : StackPanel
 
             if (child is StackPanelExDecorator decorator)
             {
-                if (decorator.Margin != Spacing)
-                    decorator.Margin = Spacing;
+                if (Math.Abs(decorator.Margin.Top - Gap) > 0.001d || Math.Abs(decorator.Margin.Left - Gap) > 0.001d)
+                    decorator.Margin = GetGap(decorator.Child, i);
 
                 continue;
             }
 
             InternalChildren.RemoveAt(i);
-            InternalChildren.Insert(i, new StackPanelExDecorator { Child = child, Margin = Spacing });
+            InternalChildren.Insert(i, new StackPanelExDecorator { Child = child, Margin = GetGap(child, i) });
         }
 
         return base.MeasureOverride(constraint);
+    }
+
+    private Thickness GetGap(UIElement child, int childIndex)
+    {
+        if (childIndex == 0) return new Thickness();
+
+        return Orientation is Orientation.Horizontal ?
+               new Thickness(Gap, 0, 0, 0) :
+               new Thickness(0, Gap, 0, 0);
     }
 }
