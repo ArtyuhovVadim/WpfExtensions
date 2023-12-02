3f10ca7a18f0c155e7f600cd180091feec5b11d2
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-12-03 00:26:57 +0300

    Добавил AdornedControl

---
+++ WpfExtencions.Controls/AdornerPlacement.cs
@@ -0,0 +1,8 @@
+﻿namespace WpfExtensions.Controls;
+
+//Source: https://www.codeproject.com/Articles/54472/Defining-WPF-Adorners-in-XAML
+public enum AdornerPlacement
+{
+    Inside,
+    Outside
+}
