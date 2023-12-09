962d07f6ff22b40d8b080fd10e93b1387d086b69
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-12-09 13:50:41 +0300

    Добавил пару расширений разметки

---
+++ WpfExtensions/MarkupExtensions/SystemFontFamilies.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Markup;
+
+namespace WpfExtensions.MarkupExtensions;
+
+public class SystemFontFamilies : MarkupExtension
+{
+    private static readonly IEnumerable<string> Fonts;
+
+    static SystemFontFamilies() => Fonts = System.Windows.Media.Fonts.SystemFontFamilies.Select(x => x.Source);
+
+    public override object ProvideValue(IServiceProvider serviceProvider) => Fonts;
+}
