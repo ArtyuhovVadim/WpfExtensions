2b36c4df1b977199c8f4e44f3a9aedb6cc71122a
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-12-17 01:38:34 +0300

    Исправил ошибки привязов

---
+++ WpfExtensions/Converters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+using WpfExtensions.Converters.Base;
+
+namespace WpfExtensions.Converters;
+
+[ValueConversion(typeof(Color), typeof(SolidColorBrush))]
+[MarkupExtensionReturnType(typeof(ColorToSolidColorBrushConverter))]
+public class ColorToSolidColorBrushConverter : BaseConverter
+{
+    public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => new SolidColorBrush((Color)value!);
+
+    public override object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => ((SolidColorBrush)value!).Color;
+};
