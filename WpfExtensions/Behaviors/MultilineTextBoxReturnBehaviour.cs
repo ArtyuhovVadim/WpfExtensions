f2932035c375c234a7d4e52861916ba4a763ae13
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-12-26 23:12:45 +0300

    Исправил баг с фокусом у ColorPicker и добавил поведение для TextBox, чтобы можно было добавлять переносы строк при нажатии Shift + Enter

---
+++ WpfExtensions/Behaviors/MultilineTextBoxReturnBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace WpfExtensions.Behaviors;
+
+public class MultilineTextBoxReturnBehaviour : Behavior<TextBox>
+{
+    protected override void OnAttached() => AssociatedObject.PreviewKeyDown += OnKeyDown;
+
+    protected override void OnDetaching() => AssociatedObject.PreviewKeyDown -= OnKeyDown;
+
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (Keyboard.Modifiers != ModifierKeys.Shift || e.Key != Key.Enter) return;
+
+        var caretIndex = AssociatedObject.CaretIndex;
+        AssociatedObject.Text = AssociatedObject.Text.Insert(caretIndex, "\n");
+        AssociatedObject.CaretIndex = caretIndex + 1;
+        e.Handled = true;
+    }
+}
