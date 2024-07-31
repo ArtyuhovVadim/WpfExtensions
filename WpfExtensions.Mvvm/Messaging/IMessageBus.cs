8e04f431658737289f93804e0a07fd01a0b691a0
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-07-31 18:58:48 +0300

    Добавил MessageBus

---
+++ WpfExtensions.Mvvm/Messaging/IMessageBus.cs
@@ -0,0 +1,15 @@
+﻿namespace WpfExtensions.Mvvm.Messaging;
+
+public interface IMessageBus
+{
+    ISubscription RegisterHandler<TRecipient, TMessage>(TRecipient recipient, Action<TRecipient, TMessage> handler, RefType refType = RefType.Weak) 
+        where TMessage : class 
+        where TRecipient : class;
+
+    ISubscription RegisterHandler<TMessage>(IRecipient<TMessage> recipient, RefType refType = RefType.Weak)
+        where TMessage : class;
+
+    bool Unregister<TMessage>(ISubscription subscription) where TMessage : class;
+
+    void Send<TMessage>(TMessage message) where TMessage : class;
+}
